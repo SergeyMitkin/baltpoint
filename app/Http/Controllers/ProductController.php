f4ee6b4c0cc8375bb25d7547c0baa40c09f940f0
SergeyMitkin <sergeymitkin@gmail.com>
2025-03-03 05:03:31 +0300

    удалён лишний код

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,24 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Brand;
 use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 use Illuminate\View\View;
 
 class ProductController extends Controller
 {
     public function index(): View
     {
-//        $clothing_types = ['Футболка', 'Джинсы', 'Свитер', 'Куртка', 'Шорты', 'Кроссовки',
-//            'Бейсболка', 'Рубашка', 'Платье', 'Брюки'];
-//
-//        dd($clothing_types[array_rand($clothing_types)]);
-//        $brand_ids = Brand::all()->pluck('id')->toArray();
-//
-//        $price = mt_rand(100, 100000) + mt_rand(0, 99) / 100;
-//
-//        dd($price);
-
-
         $products= Product::with('brand')->get();
 
         return view('index', ['products' => $products]);
